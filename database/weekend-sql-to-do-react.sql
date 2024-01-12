ffc9e24886961478c5144fc6395bfdad8932560b
Alvin Graham <grah0071@gmail.com>
2024-01-12 15:29:46 -0600

    Create initial databse

---
+++ database/weekend-sql-to-do-react.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS task_list;
+
+CREATE TABLE "task_list" (
+	"id" SERIAL PRIMARY KEY,
+	"title" varchar(80) NOT NULL,
+	"description" text,
+	"priority" varchar(10) NOT NULL
+  CHECK ("priority" ILIKE 'low' OR "priority" ILIKE 'medium' OR "priority" ILIKE 'high'),
+  "complete" boolean NOT NULL
+);
+
+INSERT INTO "task_list" ("title", "description", "priority", "complete")
+VALUES ('Urgent Task', 'I am a high priority task', 'high', 'false');
